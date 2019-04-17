832dd72fa38f393486dc90c2e642255711e57efe
Fatima Turgunbaeva <fturgunbaeva@gmail.com>
2019-04-17 17:10:18 +0600

    add new changes

---
+++ script/script.js
@@ -0,0 +1,16 @@
+let inputFrom = document.querySelector('.from-input').value
+let inputTo = document.querySelector('.to-input').value
+let inputDate = document.querySelector('.datetime').value
+let btn = document.querySelector('.button')
+
+btn.addEventListener('click', function() {
+  let data = inputFrom.value
+  console.log(data)
+})
+
+
+
+
+
+
+
